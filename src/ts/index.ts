3e978d61d5ed4edad44f320696e3e4ccef42c4ad
Satheesh P <satheeshp@success4.us>
2023-07-24 20:11:30 +0530

    music-test-done

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,27 @@
 import { notesToPlayInOrder } from "./music-to-play";
 
-function playMusic() {
+
+const BEATS_PER_MINUTE = 240;
+
+function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function playMusic() {
     const notes = notesToPlayInOrder;
-    
-    // TODO Play these notes one after the other at the pitch and rhythm given in each note
+    const playDelay = (60 / BEATS_PER_MINUTE) * 1000;
+
+    for (const note of notes) {
+        const audioId = `${note.pitch}${note.accidental || ""}${note.octave}`;
+        const audioElement = document.getElementById(audioId) as HTMLAudioElement;
+
+        if (audioElement) {
+            audioElement.play();
+            await wait(note.beats * playDelay);
+            audioElement.pause();
+            audioElement.currentTime = 0;
+        }
+    }
 }
 
 document.getElementById('start-playing')?.addEventListener('click', playMusic);
